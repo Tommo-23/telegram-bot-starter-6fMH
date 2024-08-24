8c2f7be6e49a03969e76e12f30a1b3260ca31397
Toommo23 <142088873+Tommo-23@users.noreply.github.com>
2024-08-24 21:43:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,33 +8,23 @@ TOKEN = "7514576903:AAH9eTd__xqhey_jnzwP1pLl0DTYgtckODw"
 # Canale di destinazione per l'inoltro dei messaggi
 CHAT_DESTINAZIONE = "@TuttoModding"
 
-# Canali da monitorare
-CANALI_MONITORATI = {
-    '@garnet_updates': 'rn13pro5g / PocoX6',
-    '@PocoF6GlobalUpdate': 'POCO F6',
-    '@Redmi10CUpdates': 'Redmi 10c',
-    '@setupTommo23': 'test',
-}
-
 async def start(update: Update, context: CallbackContext) -> None:
     """Risponde al comando /start."""
-    await update.message.reply_text('my job is to forward the new roms of your device.')
+    await update.message.reply_text('my job is to forward all messages to the specified channel.')
 
 async def inoltra_messaggio(update: Update, context: CallbackContext) -> None:
-    """Inoltra il messaggio se proviene da un canale monitorato e logga dettagli."""
+    """Inoltra tutti i messaggi al canale di destinazione e logga dettagli."""
     if update.message:
-        chat_username = update.message.chat.username
-        message_text = update.message.text
+        chat = update.message.chat
+        chat_username = chat.username if chat.username else 'N/A'
+        message_text = update.message.text if update.message.text else 'No text'
         print(f"Ricevuto messaggio da: {chat_username}")
         print(f"Dettagli del messaggio: {update.message.to_dict()}")
 
-        if chat_username in CANALI_MONITORATI:
-            nome_canale = CANALI_MONITORATI[chat_username]
-            testo_inoltrato = f"Nuova ROM per {nome_canale}\n\n{message_text}"
-            await context.bot.send_message(chat_id=CHAT_DESTINAZIONE, text=testo_inoltrato)
-            print(f"Inoltrato messaggio: {testo_inoltrato}")
-        else:
-            print("L'aggiornamento ricevuto non proviene da un canale monitorato.")
+        # Costruisci il testo del messaggio da inoltrare
+        testo_inoltrato = f"Messaggio da {chat_username}:\n\n{message_text}"
+        await context.bot.send_message(chat_id=CHAT_DESTINAZIONE, text=testo_inoltrato)
+        print(f"Inoltrato messaggio: {testo_inoltrato}")
     else:
         print("L'aggiornamento ricevuto non contiene un messaggio valido.")
 
@@ -44,7 +34,7 @@ def main() -> None:
 
     # Aggiungi i gestori di comandi e messaggi
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.ChatType.CHANNEL, inoltra_messaggio))
+    application.add_handler(MessageHandler(filters.ALL, inoltra_messaggio))
 
     # Esegui l'applicazione utilizzando il metodo di polling o webhook
     if os.getenv("USE_WEBHOOK") == "True":
@@ -52,7 +42,7 @@ def main() -> None:
             listen="0.0.0.0",
             port=int(os.environ.get("PORT", 8443)),
             url_path=TOKEN,
-            webhook_url="https://telegram-bot-starter-production-f0c4.up.railway.app/" + TOKEN,
+            webhook_url=f"https://telegram-bot-starter-production-f0c4.up.railway.app/{TOKEN}",
         )
     else:
         application.run_polling()
