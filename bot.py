b7db3c57028035d04ae50fe5a7c627a15fe425df
Toommo23 <142088873+Tommo-23@users.noreply.github.com>
2024-08-24 13:02:18 +0200

    gffff

--- bot.py
+++ bot.py
@@ -1,75 +1,41 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
-import logging
 import os
+import asyncio
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.error import NetworkError
 
-from dotenv import load_dotenv
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+TOKEN = os.getenv("TOKEN")
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+CHAT_DESTINAZIONE = "@TuttoModding"  # Canale di destinazione
 
-logger = logging.getLogger(__name__)
+CANALI_MONITORATI = {
+    '@garnet_updates': 'rn13pro5g / PocoX6',
+    '@PocoF6GlobalUpdate': 'POCO F6',
+    '@Redmi10CUpdates': 'Redmi 10c',    
+    '@setupTommo23': 'test',    
+}
 
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('my job is to forward the new roms of your device.')
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
+async def inoltra_messaggio(update: Update, context: CallbackContext) -> None:
+    if update.message and update.message.chat.username in CANALI_MONITORATI:
+        try:
+            await update.message.forward(chat_id=CHAT_DESTINAZIONE)
+        except NetworkError as e:
+            print(f"Network error occurred: {e}. Retrying in 5 seconds...")
+            await asyncio.sleep(5)
+            await update.message.forward(chat_id=CHAT_DESTINAZIONE)
+    else:
+        print("L'aggiornamento ricevuto non proviene da un canale monitorato.")
 
 def main() -> None:
-    """Start the bot."""
-    load_dotenv()
+    application = Application.builder().token(TOKEN).build()
 
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(os.environ["TOKEN"]).build()
-
-    # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(MessageHandler(filters.ChatType.CHANNEL, inoltra_messaggio))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.run_polling()
 
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
